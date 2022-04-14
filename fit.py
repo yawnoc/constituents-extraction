e142761c4762ec38d4825a2169897e69c1787664
Conway <yawnoc@pm.me>
2022-04-15 00:03:49 +0800

    Implement loading of data

--- fit.py
+++ fit.py
@@ -4,12 +4,29 @@
 # fit.py
 
 Do fitting to extract Monomer and Aggregate constituents
-of a biological sample.
+for {frame number} vs {average intensity} data.
 """
 
 
+import os
+import numpy as np
+
+
+DATA_DIRECTORY = 'data'
+
+
+def load_data():
+  data_from_file_name = {}
+  for path, _, file_names in os.walk(DATA_DIRECTORY):
+    for file_name in file_names:
+      full_file_name = os.path.join(path, file_name)
+      data_from_file_name[file_name] = np.loadtxt(full_file_name, skiprows=1)
+  return data_from_file_name
+
+
 def main():
-  pass
+  data_from_file_name = load_data()
+  print(data_from_file_name)
 
 
 if __name__ == '__main__':
