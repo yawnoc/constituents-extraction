7c58306cb3299d58e1a9b011cf1a56fb9ab34da8
Conway <yawnoc@pm.me>
2022-04-15 00:36:31 +0800

    Load data in order of file name

--- fit.py
+++ fit.py
@@ -20,7 +20,7 @@ DATA_DIRECTORY = 'data'
 def load_data():
   data_from_file_name = {}
   for path, _, file_names in os.walk(DATA_DIRECTORY):
-    for file_name in file_names:
+    for file_name in sorted(file_names):
       full_file_name = os.path.join(path, file_name)
       data_from_file_name[file_name] = np.loadtxt(full_file_name, skiprows=1)
   return data_from_file_name
