cdb051c2b83f4a25aef2dfe38fd812af5150669a
Conway <yawnoc@pm.me>
2022-04-14 23:48:15 +0800

    Create fit.py

---
+++ fit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+# fit.py
+
+Do fitting to extract Monomer and Aggregate constituents
+of a biological sample.
+"""
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
