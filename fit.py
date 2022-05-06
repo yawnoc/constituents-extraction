d48c5f1a5322866acb8dd72bbfb906603d2d48d1
Conway <yawnoc@pm.me>
2022-05-06 22:35:38 +0800

    Rewrite data normalisation

--- fit.py
+++ fit.py
@@ -33,11 +33,28 @@ def load_data_points():
   return data_points_from_file_name
 
 
+def normalise(data):
+  """
+  Normalise data (for a variable) to between 0 and 1.
+  """
+  
+  data_min = data.min()
+  data_max = data.max()
+  normalised_data = (data - data_min) / (data_max - data_min)
+  
+  return data_min, data_max, normalised_data
+
+
 def main():
   
   data_points_from_file_name = load_data_points()
   for file_name, data_points in data_points_from_file_name.items():
-    pass
+    
+    frame_data = data_points[:, 0]
+    intensity_data = data_points[:, 1]
+    
+    frame_min, frame_max, x_data = normalise(frame_data)
+    intensity_min, intensity_max, y_data = normalise(intensity_data)
 
 
 if __name__ == '__main__':
