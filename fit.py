257fd5f1098f44d50dfa8c80b413a971a44d1278
Conway <yawnoc@pm.me>
2022-06-19 11:01:28 +0800

    Add standard errors to output

--- fit.py
+++ fit.py
@@ -370,11 +370,7 @@ def main():
       ################################
       # 2-peak fit
       ################################
-      (
-        m_fit, b_fit,
-        a1_fit, mu1_fit, sigma1_fit, tau1_fit,
-        a2_fit, mu2_fit, sigma2_fit, tau2_fit,
-      ), _ = \
+      fit_parameters, fit_covariances = \
               opt.curve_fit(
                 two_peak_model,
                 x_data, y_data,
@@ -384,6 +380,18 @@ def main():
                   a2_guess, mu2_guess, sigma2_guess, tau2_guess,
                 )
               )
+      (
+        m_fit, b_fit,
+        a1_fit, mu1_fit, sigma1_fit, tau1_fit,
+        a2_fit, mu2_fit, sigma2_fit, tau2_fit,
+      ) = \
+              fit_parameters
+      (
+        m_fit_error, b_fit_error,
+        a1_fit_error, mu1_fit_error, sigma1_fit_error, tau1_fit_error,
+        a2_fit_error, mu2_fit_error, sigma2_fit_error, tau2_fit_error,
+      ) = \
+              np.sqrt(np.diag(fit_covariances))
       y_fit = \
               two_peak_model(
                 x_data,
@@ -401,6 +409,7 @@ def main():
       y_peak2_fit_with_background = y_peak2_fit + y_background_fit
       
       a_fit = mu_fit = sigma_fit = tau_fit = None
+      a_fit_error = mu_fit_error = sigma_fit_error = tau_fit_error = None
       y_peak_fit_with_background = None
     
     except ValueError: # 1 peak
@@ -433,12 +442,18 @@ def main():
       ################################
       # 1-peak fit
       ################################
-      (m_fit, b_fit, a_fit, mu_fit, sigma_fit, tau_fit), _ = \
+      fit_parameters, fit_covariances = \
               opt.curve_fit(
                 one_peak_model,
                 x_data, y_data,
                 (m_guess, b_guess, a_guess, mu_guess, sigma_guess, tau_guess),
               )
+      m_fit, b_fit, a_fit, mu_fit, sigma_fit, tau_fit = fit_parameters
+      (
+        m_fit_error, b_fit_error,
+        a_fit_error, mu_fit_error, sigma_fit_error, tau_fit_error,
+      ) = \
+              np.sqrt(np.diag(fit_covariances))
       y_fit = \
               one_peak_model(
                 x_data,
@@ -452,6 +467,8 @@ def main():
       
       a1_fit = mu1_fit = sigma1_fit = tau1_fit = None
       a2_fit = mu2_fit = sigma2_fit = tau2_fit = None
+      a1_fit_error = mu1_fit_error = sigma1_fit_error = tau1_fit_error = None
+      a2_fit_error = mu2_fit_error = sigma2_fit_error = tau2_fit_error = None
       y_peak1_fit_with_background = y_peak2_fit_with_background = None
     
     ################################
@@ -613,33 +630,33 @@ def main():
       tsv_writer.writerow(['intensity_max', intensity_max])
       tsv_writer.writerow([])
       
-      tsv_writer.writerow(['# Background'])
-      tsv_writer.writerow(['m', m_fit])
-      tsv_writer.writerow(['b', b_fit])
+      tsv_writer.writerow(['# Background', 'Estimate', 'Standard Error'])
+      tsv_writer.writerow(['m', m_fit, m_fit_error])
+      tsv_writer.writerow(['b', b_fit, b_fit_error])
       tsv_writer.writerow([])
       
       if a_fit is not None:
-        tsv_writer.writerow(['# Peak'])
-        tsv_writer.writerow(['A', a_fit])
-        tsv_writer.writerow(['mu', mu_fit])
-        tsv_writer.writerow(['sigma', sigma_fit])
-        tsv_writer.writerow(['tau', tau_fit])
+        tsv_writer.writerow(['# Peak', 'Estimate', 'Standard Error'])
+        tsv_writer.writerow(['A', a_fit, a_fit_error])
+        tsv_writer.writerow(['mu', mu_fit, mu_fit_error])
+        tsv_writer.writerow(['sigma', sigma_fit, sigma_fit_error])
+        tsv_writer.writerow(['tau', tau_fit, tau_fit_error])
         tsv_writer.writerow([])
       
       if a1_fit is not None:
         tsv_writer.writerow(['# Peak 1'])
-        tsv_writer.writerow(['A1', a1_fit])
-        tsv_writer.writerow(['mu1', mu1_fit])
-        tsv_writer.writerow(['sigma1', sigma1_fit])
-        tsv_writer.writerow(['tau1', tau1_fit])
+        tsv_writer.writerow(['A1', a1_fit, a1_fit_error])
+        tsv_writer.writerow(['mu1', mu1_fit, mu1_fit_error])
+        tsv_writer.writerow(['sigma1', sigma1_fit, sigma1_fit_error])
+        tsv_writer.writerow(['tau1', tau1_fit, tau1_fit_error])
         tsv_writer.writerow([])
       
       if a2_fit is not None:
-        tsv_writer.writerow(['# Peak 2'])
-        tsv_writer.writerow(['A2', a2_fit])
-        tsv_writer.writerow(['mu2', mu2_fit])
-        tsv_writer.writerow(['sigma2', sigma2_fit])
-        tsv_writer.writerow(['tau2', tau2_fit])
+        tsv_writer.writerow(['# Peak 2', 'Estimate', 'Standard Error'])
+        tsv_writer.writerow(['A2', a2_fit, a2_fit_error])
+        tsv_writer.writerow(['mu2', mu2_fit, mu2_fit_error])
+        tsv_writer.writerow(['sigma2', sigma2_fit, sigma2_fit_error])
+        tsv_writer.writerow(['tau2', tau2_fit, tau2_fit_error])
         tsv_writer.writerow([])
       
       if a1_fit is not None and a2_fit is not None:
